24c74ef40ad8b3d8b738308f6b267782f98eb96e
Mai Thanh Nhã <nhamt0306@gmail.com>
2021-10-21 18:48:43 +0700

    done

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => { 
-    return res.send('Hello World!')
+  var a = 1;
+  var b=2;
+  var sum = a+b;
+  return res.send('Hello World!')
 })
 
 app.listen(port, () => {
