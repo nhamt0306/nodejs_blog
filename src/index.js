353c4fbacb01ca46b07ce84bf5a1266eb65a8826
Mai Thanh Nhã <nhamt0306@gmail.com>
2021-10-21 21:57:12 +0700

    commit full layout

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars  = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//Để sử dụng được đường dẫn tĩnh
+app.use(express.static(path.join(__dirname,'public'))); //Lúc này để truy cập chỉ cần /f8_icon.png
+
+//Template engine - handlebars
+app.engine('hbs', handlebars({
+  extname : ".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources\\views'))
+
+//HTTP logger - Morgan
+app.use(morgan('combined'));
+
+
+app.get('/', (req, res) => { 
+  res.render('home');
+})
+
+app.get('/news', (req, res) => { 
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
