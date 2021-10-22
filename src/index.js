5ee424b89110dcde5a1035cc85c8a2f0c900812a
Mai Thanh Nhã <nhamt0306@gmail.com>
2021-10-23 01:51:23 +0700

    ''get and post done!"

--- src/index.js
+++ src/index.js
@@ -21,11 +21,15 @@ app.use(morgan('combined'));
 
 app.get('/', (req, res) => { 
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => { 
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => { 
+  res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
